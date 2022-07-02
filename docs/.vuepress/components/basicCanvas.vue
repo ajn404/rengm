e381c25f6807bb0459108f07a1c00bbbc7869b58
mac_ajn404 <huiyuening00@gmail.com>
2022-07-03 01:03:16 +0800

    no message

---
+++ docs/.vuepress/components/basicCanvas.vue
@@ -0,0 +1,47 @@
+
+<template>
+    <div class="box">
+        <canvas id="basic-canvas"></canvas>
+        <el-button @click="changeLoop">{{ope}}</el-button>
+    </div>
+</template>
+<script lang='ts' setup>
+import { ref, nextTick ,onUnmounted,toRef } from "vue";
+import * as canvasFunc from "../common/canvasFuncs.js"
+
+
+const draw = () => {
+     window.p5DrawLoop = "particles"
+    canvasFunc.particles()
+}
+nextTick(() => {
+    draw()
+})
+
+onUnmounted(()=>{
+    window.p5DrawLoop  = ""
+})
+
+const ope = ref("停止")
+
+const changeLoop = ()=>{
+    window.p5DrawLoop = window.p5DrawLoop?"":"particles"
+    ope.value = p5DrawLoop?"停止":"重启"
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+.box{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+}
+canvas {
+    width: 100%;
+    height: 100%;
+    display: block;
+}
+</style>
