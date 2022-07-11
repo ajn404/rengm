455d61d997dd73cdcf5b8fa563216885adef59ab
ajn404 <huiyuening00@gmail.com>
2022-07-11 17:30:57 +0800

    no message

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,20 +1,25 @@
 <script setup>
-    import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
-    import pageTop from './pageTop.vue'
-    import pageBottom from "./pageBottom.vue";
-    import {ref} from 'vue'
-    import {ElMessage} from 'element-plus'
+import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+import pageTop from './pageTop.vue'
+import pageBottom from "./pageBottom.vue";
+import { ref, onActivated, nextTick,onMounted,watchEffect,watch } from 'vue'
+import { ElMessage } from 'element-plus'
 
-    const style = ref(true)
-    const click = () => {
-        style.value = !style.value
-        // ElMessage.warning("没有做持久化，刷新还是会重置")
-    }
+const style = ref(true)
+const click = () => {
+    style.value = !style.value
+    // ElMessage.warning("没有做持久化，刷新还是会重置")
+}
 
-    import {useRoute} from 'vue-router'
+import { useRoute } from 'vue-router'
 
-    const route = useRoute()
-    const showCustomLayout = route.meta.customLayout
+const route = useRoute()
+let showCustomLayout = ref(false); 
+
+watch(route,()=>{
+    showCustomLayout.value = route.meta.customLayout == false ? false : true
+})
+
 </script>
 <template>
     <ParentLayout>
@@ -22,39 +27,34 @@
         <template #sidebar-bottom></template>
         <template #navbar-before></template>
         <template #navbar-after>
-            <el-button
-                    v-if="showCustomLayout"
-                    text
-                    @click="click"
-                    class="change-style">删除插槽
-            </el-button>
+            <el-button v-if="showCustomLayout" text @click="click" class="change-style">删除插槽</el-button>
         </template>
         <template #page-bottom>
-            <page-bottom v-if="showCustomLayout&&style"></page-bottom>
+            <page-bottom v-if="showCustomLayout && style"></page-bottom>
         </template>
         <template #page-top>
-            <page-top v-if="showCustomLayout&&style"></page-top>
+            <page-top v-if="showCustomLayout && style"></page-top>
         </template>
     </ParentLayout>
 </template>
 
 <style lang="scss" scoped>
-    .my-footer {
-        text-align: center;
-    }
+.my-footer {
+    text-align: center;
+}
 
-    .change-style {
-        padding: 0;
-        margin-left: 1.5rem;
-        height: 100%;
-        font-size: .9rem;
-        color: var(--c-text);
-        line-height: 1.4rem;
-        background: var(--c-bg) !important;
-        // transition-duration: .5s;
-        &:hover {
-            // transform: rotate(360deg);
-            color: var(--c-brand);
-        }
+.change-style {
+    padding: 0;
+    margin-left: 1.5rem;
+    height: 100%;
+    font-size: 0.9rem;
+    color: var(--c-text);
+    line-height: 1.4rem;
+    background: var(--c-bg) !important;
+    // transition-duration: .5s;
+    &:hover {
+        // transform: rotate(360deg);
+        color: var(--c-brand);
     }
+}
 </style>
--- docs/basic/css/cssDemo.md
+++ docs/basic/css/cssDemo.md
@@ -6,7 +6,20 @@ customLayout : false
 }
 ---
 
+## demo-1 flex 布局
+
+flexbox 是 一种一维布局，只能处理一行或者一列。
+
 <ClientOnly>
-<css-demo></css-demo>
+<css-demo cssPractice="demo1-1"></css-demo>
 </ClientOnly>
 
+## 写基础的好生无趣，放飞了
+
+::: details scss核心代码
+@[code scss{18-41}:no-line-numbers](../../.vuepress/components/template/cssDemos/Demo2_1.vue))
+:::
+
+<ClientOnly>
+<css-demo cssPractice="demo2-1"></css-demo>
+</ClientOnly>
