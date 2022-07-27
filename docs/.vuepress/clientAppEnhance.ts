38e1a09a12b8732ce22c838d2b97af9c68455079
mac_ajn404 <huiyuening00@gmail.com>
2022-07-27 11:43:45 +0800

    no message

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -1,6 +1,6 @@
 import ElementPlus,{ElMessage} from 'element-plus'
 import "element-plus/dist/index.css"
-// import "@/public/styles/index.scss"
+import "@/public/styles/index.scss"
 import store from './store/index.js'
 import { defineClientConfig } from '@vuepress/client'
 console.log('%crengm\n%crewrite ngm','color:yellow;font-size:3em;','color:red;font-size:1.5em')
